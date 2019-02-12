0d49ace9d051e2f5043f99f3098c38a2b2d28f21
user-penguin <kobzev96@gmail.com>
2019-02-13 03:06:01 +0700

    add Singleton

--- Kitchen/src/model/SingletonKitchen.java
+++ Kitchen/src/model/SingletonKitchen.java
@@ -1,4 +1,17 @@
 package model;
 
-public class SingletonKitchen {
+public final class SingletonKitchen {
+    private static SingletonKitchen instance;
+    public int countOfBake;
+
+    private SingletonKitchen(int countOfBake) {
+        this.countOfBake = countOfBake;
+    }
+
+    public static SingletonKitchen getInstance(int countOfBake) {
+        if (instance == null) {
+            instance = new SingletonKitchen(countOfBake);
+        }
+        return instance;
+    }
 }
