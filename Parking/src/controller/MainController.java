8b19b638267e63a52d3f596ddc56492390d6a9ac
user-penguin <kobzev96@gmail.com>
2019-02-13 15:30:21 +0700

    initial

---
+++ Parking/src/controller/MainController.java
@@ -0,0 +1,9 @@
+package controller;
+
+import view.TUI;
+
+public class MainController {
+    public static void main(String[] args) {
+        TUI.printMainMenu();
+    }
+}
---
+++ Parking/src/model/Car.java
@@ -0,0 +1,10 @@
+package model;
+
+public interface Car {
+    public void startEngine();
+    public void stopEngine();
+    public void runAhead();
+    public void stop();
+    public void turnLeft();
+    public void turnRight();
+}
